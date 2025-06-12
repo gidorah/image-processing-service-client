d0501b2c5c0fda27929ad8003251910e29ed63e7
Onur Akyüz <onur_akyuz@icloud.com>
2025-06-12 16:32:04 +0300

    Set up Prettier configuration and update ESLint settings
    - Added .prettierrc.cjs for Prettier configuration with Tailwind CSS plugin.
    - Updated eslint.config.mjs to include eslint-config-prettier for better integration with Prettier.
    - Modified package.json and package-lock.json to include Prettier and related dependencies.
    - Added new linting and formatting scripts to package.json for improved code quality.

---
+++ .prettierrc.cjs
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  semi: true,
+  tabWidth: 2,
+  singleQuote: false,
+  trailingComma: "es5",
+  plugins: ["prettier-plugin-tailwindcss"],
+};
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/shared/header";
@@ -29,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="relative flex min-h-screen flex-col bg-background">
+        <div className="bg-background relative flex min-h-screen flex-col">
           <Header />
           <main className="flex-1">{children}</main>
           <Footer />
